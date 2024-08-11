fde57f132b27821052547c0435d622373d7ba509
João Henrique <jhenrique.caval@hotmail.com>
2024-08-11 13:19:52 -0300

    ajustes no script

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 
 const form = document.getElementById("form")
+let linhas = '';
+
  form.addEventListener('submit', function(e){
         e.preventDefault();
  
         const inputNomeAtividade = document.getElementById("activityName");
         const inputNotaAtividade = document.getElementById("nota");
          
+
         let linha = "<tr>";
         linha += `<td>${inputNomeAtividade.value}</td>`;
         linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= 7 ? "aprovado" : "reprovado"}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= 7 ? "aprovado!" : "reprovado."}</td>`;
         linha += "</tr>";
-        
+
+        linhas += linha;
+
         const corpoTabela = document.querySelector("tbody");
-        corpoTabela.innerHTML = linha;
+        corpoTabela.innerHTML = linhas;
 
     });
 
