98dc056596b82f01773da60857a277a3097c7ea4
João Henrique <jhenrique.caval@hotmail.com>
2024-08-11 13:16:56 -0300

    ajustando js

--- main.js
+++ main.js
@@ -2,14 +2,22 @@
 const form = document.getElementById("form")
  form.addEventListener('submit', function(e){
         e.preventDefault();
-        const atividadeNome = document.getElementById('activityName').value;
-        const nota = document.getElementById('nota').value;
-        let notas = []
-        document.getElementById('nota').value = ""
-        document.getElementById('activityName').value = ""
-        document.getElementById('tablealuno').innerHTML += `<tr>
-        <td>${atividadeNome}</td>
-        <td> ${nota}</td>
-        </tr>`
-        const table = document.querySelector(".tableDiv")
-    })
+ 
+        const inputNomeAtividade = document.getElementById("activityName");
+        const inputNotaAtividade = document.getElementById("nota");
+         
+        let linha = "<tr>";
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= 7 ? "aprovado" : "reprovado"}</td>`;
+        linha += "</tr>";
+        
+        const corpoTabela = document.querySelector("tbody");
+        corpoTabela.innerHTML = linha;
+
+    });
+
+
+
+ 
+
